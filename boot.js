6a1a586b7e53bac8795a1f7b5afca6b405a4d398
Domenico-lel <domenicolella2@gmail.com>
2023-11-23 21:38:48 +0100

    creazione struttura html e inizzializazione script

--- boot.js
+++ boot.js
@@ -4,29 +4,18 @@ Per prima cosa, creiamo il markup statico: l’html con tutto il necessario perc
 MILESTONE 2
 Al click dell’utente sulle frecce (sia Prev che Next), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.*/
 
-const items = document.getElementsByClassName('item');
+// dichiariamo quante immagini abbiamo
+var n_img = 5;
 
-let activeItem = 0;
+// dichiariamo la prima immagine da visualizzare
+var corrente = 1;
 
-// navigazione
-const next = document.querySelector('.next');
+// creazione funzione per mandare avanti le immagini
+function succ(){
 
-next.addEventListener('click', function() {
+}
 
-
-    if (activeItem <  items.length - 1){
-        // togliamo la classe active  all' elemento corrente
-        items[activeItem].classList.remove('active');
-
-        // incrementiamo l' indice dell' elemento da visualizzare
-        activeItem++;
-
-        // aggiungiamo la classe active all' elemento successivo 
-        items[activeItem].classList.add('active');
-
-        if (activeItem === items.length - 1){
-            // siamo arrivati all' ultimo elemento
-            next.classList.add('hidden')
-        }
-    }
-})
+// creazione funzione per mandare in dietro le immagini
+function prec(){
+    
+}
